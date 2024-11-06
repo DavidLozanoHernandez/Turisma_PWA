691c8bf21ccc5ed51cb5311819927b502738414d
Perry <davidlh257@gmail.com>
2024-11-06 11:01:59 -0600

    Agregar imagen a la app PWA

--- js/api.js
+++ js/api.js
@@ -1,6 +1,5 @@
 const BASE_URL = 'http://localhost:3000';
 
-// Función para enviar credenciales de inicio de sesión
 async function login(credentials) {
     console.log(credentials)
   try {
@@ -20,29 +19,25 @@ async function login(credentials) {
     }
 
     const data = await response.json();
-    return data; // Devuelve los datos del servidor, como un token de autenticación
+    return data;
   } catch (error) {
     console.error('Error al intentar iniciar sesión:', error);
     throw error;
   }
 }
 
-// Manejo del evento de envío del formulario
 document.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.getElementById('loginForm');
 
   loginForm.addEventListener('submit', async (event) => {
     event.preventDefault(); // Evita el envío normal del formulario
 
-    // Captura los datos del formulario
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    // Prepara las credenciales en el formato esperado
     const credentials = { email, password };
 
     try {
-      // Llama a la función login para enviar las credenciales
       const response = await login(credentials);
       console.log('Inicio de sesión exitoso:', response);
       
